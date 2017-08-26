4cef0b1bd80985084c2f33d3ef306e4a18362c29
sonyanai <taison0930@outlook.jp>
2017-08-26 22:50:20 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/taison/yanai/wings/MainActivity.java
@@ -0,0 +1,36 @@
+package jp.techacademy.taison.yanai.wings;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.item_receive:
+                    setFragment();
+                    return true;
+                case R.id.item_send:
+                    setFragment();
+                    return true;
+                case R.id.item_realm:
+                    setFragment();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
